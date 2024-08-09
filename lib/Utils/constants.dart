62c5cc118c3a0e04179ec2938b50f7dc98a3bd97
bora399 <borasaltik_39@hotmail.com>
2024-08-09 23:39:17 +0300

    Ux Designed, constants.dart, widget_functions.dart created, main.dart cleaned.

---
+++ lib/Utils/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color colorGray = Color.fromARGB(255, 140, 160, 176);
+const Color colorBlue = Color.fromARGB(255, 29, 144, 245);
+const Color colorBackground = Color.fromARGB(255, 39, 42, 55);
+const Color colorButton = Color.fromARGB(255, 64, 69, 86);
+const Color colorIcon = Color.fromARGB(255, 62, 150, 146);
+const Color colorOrange = Color.fromARGB(255, 223, 64, 70);
+const Color colorProfit = Color.fromARGB(255, 43, 196, 53);
+const Color colorLoss = Color.fromARGB(255, 198, 43, 43);
+
+const TextTheme textThemeDefault = TextTheme(
+
+);
